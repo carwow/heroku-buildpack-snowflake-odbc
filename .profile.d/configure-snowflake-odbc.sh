c560c63183b5ee8c4150a2b466dd196b8e7cddd9
Pedro Chambino <pchambino@gmail.com>
2025-08-07 12:42:17 +0100

    Use SNOWFLAKE_PRIV_KEY when available
    This should replace the use of Snowflake passwords.

--- .profile.d/configure-snowflake-odbc.sh
+++ .profile.d/configure-snowflake-odbc.sh
@@ -12,3 +12,11 @@ PORT=443
 SSL=on
 CLIENT_SESSION_KEEP_ALIVE=true
 " > ${ODBCSYSINI}/odbc.ini
+
+if [ -n "${SNOWFLAKE_PRIV_KEY}" ]; then
+  echo ${SNOWFLAKE_PRIV_KEY} | base64 -d > ${ODBCSYSINI}/rsa_key.p8
+  echo "
+AUTHENTICATOR=SNOWFLAKE_JWT
+PRIV_KEY_FILE=${ODBCSYSINI}/rsa_key.p8
+" > ${ODBCSYSINI}/odbc.ini
+fi
