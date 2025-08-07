4da2f7752fa77c2c4e762137ccd631398ec828fa
Pedro Chambino <pchambino@gmail.com>
2025-08-07 16:28:46 +0100

    Append PRIV_KEY_FILE instead of replacing the config

--- .profile.d/configure-snowflake-odbc.sh
+++ .profile.d/configure-snowflake-odbc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export ODBCSYSINI=${HOME}/.apt/usr/lib/snowflake/odbc/conf/
+export ODBCSYSINI=${HOME}/.apt/usr/lib/snowflake/odbc/conf
 
 mkdir -p /etc
 echo "[snowflake]
@@ -10,13 +10,10 @@ Locale=en-US
 SERVER=${SNOWFLAKE_ACCOUNT}.snowflakecomputing.com
 PORT=443
 SSL=on
-CLIENT_SESSION_KEEP_ALIVE=true
-" > ${ODBCSYSINI}/odbc.ini
+CLIENT_SESSION_KEEP_ALIVE=true" > ${ODBCSYSINI}/odbc.ini
 
 if [ -n "${SNOWFLAKE_PRIV_KEY}" ]; then
-  echo ${SNOWFLAKE_PRIV_KEY} | base64 -d > ${ODBCSYSINI}/rsa_key.p8
-  echo "
-AUTHENTICATOR=SNOWFLAKE_JWT
-PRIV_KEY_FILE=${ODBCSYSINI}/rsa_key.p8
-" > ${ODBCSYSINI}/odbc.ini
+  echo "${SNOWFLAKE_PRIV_KEY}" | base64 -d > ${ODBCSYSINI}/rsa_key.p8
+  echo "AUTHENTICATOR=SNOWFLAKE_JWT
+PRIV_KEY_FILE=${ODBCSYSINI}/rsa_key.p8" >> ${ODBCSYSINI}/odbc.ini
 fi
